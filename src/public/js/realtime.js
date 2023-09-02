f7436002c707d20d17036e7558061a3581522d79
Antony F Montesinos <afmm101294@gmail.com>
2023-09-01 22:23:20 -0300

    desafio 4 listo para corregir

--- src/public/js/realtime.js
+++ src/public/js/realtime.js
@@ -56,7 +56,39 @@ deleteProduct = (id) => {
 
 socketClient.on("updatedProducts", data => {
     const productsContainer = document.querySelector('.bodyRealTimeProducts'); 
-    productsContainer.innerHTML = ''; 
+    productsContainer.innerHTML = `
+    <div class="container mt-5">
+    <h1 class="main-title">¡Crea un Producto Nuevo!</h1>
+    <form>
+      <div class="form-group">
+        <label for="title">Título:</label>
+        <input type="text" class="form-control custom-input" id="title" name="title" required>
+      </div>
+      <div class="form-group">
+        <label for="description">Descripción:</label>
+        <textarea class="form-control custom-input" id="description" name="description" required></textarea>
+      </div>
+      <div class="form-group">
+        <label for="price">Precio:</label>
+        <input type="number" step="0.01" class="form-control custom-input" id="price" name="price" required>
+      </div>
+      <div class="form-group">
+        <label for="code">Código:</label>
+        <input type="text" class="form-control custom-input" id="code" name="code" required>
+      </div>
+      <div class="form-group">
+        <label for="stock">Stock:</label>
+        <input type="number" class="form-control custom-input" id="stock" name="stock" required>
+      </div>
+      <div class="form-group">
+        <label for="category">Categoría:</label>
+        <textarea class="form-control custom-input" id="category" name="category" required></textarea>
+      </div>
+
+      <button id="createBtn" type="button" class="btn custom-button">Crear Producto</button>
+    </form>
+  </div>
+    `; 
 
     data.forEach(product => {
         const div = document.createElement('div');
